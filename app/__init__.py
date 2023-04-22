421fbb27852b064bcf7406b14c563bb6808c079e
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-04-22 16:03:33 -0500

    getting the project MVP stood up

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
 import os
 
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploaded_files') # this should be an env variable, but meh.
+
+import os
+
+def create_app():
+
+    app = Flask(__name__)
+    UPLOAD_FOLDER = 'uploaded_files'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+    return app
+
+app = create_app()
---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, request, send_from_directory
+import os
+from . import app
+
+bp = Blueprint('routes', __name__, url_prefix='/')
+
+@bp.route('/', methods=['GET'])
+def index():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', files=files)
+
+@bp.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return 'File uploaded and saved.', 200
+
+@bp.route('/download/<path:filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
